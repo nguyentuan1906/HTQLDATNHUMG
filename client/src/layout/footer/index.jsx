be4aaa3ebbd05178edc7bd1f6ad491fa4b825fcc
Trang007 <dangthuytrang007@gmail.com>
2022-08-21 08:52:07 +0700

    footer

---
+++ client/src/layout/footer/index.jsx
@@ -0,0 +1,16 @@
+// rafce
+import React from 'react'
+import styles from "./footer-public.module.scss"
+const Footer = () => {
+  return (
+    <div>
+        <div className={styles["footer"]}>
+           <p></p>
+
+        </div>
+    </div>
+    
+  )
+}
+
+export default Footer
--- client/src/layout/index.jsx
+++ client/src/layout/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../layout/header'
+import Footer from '../layout/footer'
 const Layout = () => {
   return (
     <div>
         <Header/>
         <Outlet/>
+        <Footer/>
     </div>
   )
 }
