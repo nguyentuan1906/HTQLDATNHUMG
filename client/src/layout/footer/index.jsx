a56aa28bb0f037e4496cc7ce88e663e0a783c671
Trang007 <dangthuytrang007@gmail.com>
2022-08-28 15:51:54 +0700

    footer new

--- client/src/layout/footer/index.jsx
+++ client/src/layout/footer/index.jsx
@@ -15,82 +15,103 @@ import {
 
 const Footer = () => {
   const { t } = useTranslation();
-  const shareUrl = 'http://github.com';
-  const title = 'GitHub';
+  const shareUrl = "http://github.com";
+  const title = "GitHub";
+  const listPage = [
+    {
+      id: 1,
+      name: "Home",
+      link: "/",
+    },
+    {
+      id: 2,
+      name: "Khoa",
+      link: "/",
+    },
+    {
+      id: 3,
+      name: "Bộ môn",
+      link: "/",
+    },
+    {
+      id: 4,
+      name: "Giảng viên",
+      link: "/",
+    },
+    {
+      id: 5,
+      name: "Sinh viên",
+      link: "/",
+    },
+  ];
   return (
     <div>
-
-    <div className={styles["footer"]}>
-      <div className={styles["h_footer"]}>
-        <div className={styles["h1_footer"]}>{t("h1_footer")}</div>
-        <div className={styles["h2_footer"]}>{t("h2_footer")}</div>
-        <div className={styles["h3_map"]}>
-        <img src="./map1.png" alt="" />
-        </div>
-      </div>
-
-      <div className={styles["footer_mxh"]}>
-        <div className={styles["f1_footer_mxh"]}>
-          {t("f1_footer_mxh")}
-          <div className={styles["f1_mxh_icon"]}>
-            <FacebookShareButton
-              url={shareUrl}
-              quote={title}
-              className="Demo__some-network__share-button"
-            >
-              <FacebookIcon size={45} round />
-            </FacebookShareButton>
-
-            <EmailShareButton
-              url={shareUrl}
-              quote={title}
-              className="Demo__some-network__share-button"
-            >
-              <EmailIcon size={45} round />
-            </EmailShareButton>
-
-            <TwitterShareButton
-              url={shareUrl}
-              quote={title}
-              className="Demo__some-network__share-button"
-            >
-              <TwitterIcon size={45} round />
-            </TwitterShareButton>
-
-            <TelegramShareButton
-              url={shareUrl}
-              quote={title}
-              className="Demo__some-network__share-button"
-            >
-              <TelegramIcon size={45} round />
-            </TelegramShareButton>
-
+      <div className={styles["footer"]}>
+        <div className={styles["h_footer"]}>
+          <div className={styles["h1_footer"]}>{t("h1_footer")}</div>
+          <div className={styles["h2_footer"]}>{t("h2_footer")}</div>
+          <div className={styles["h3_map"]}>
+            <img src="./map1.png" alt="" />
           </div>
         </div>
-      </div>
 
-      <div className={styles["footer_pt"]}>
-        <div className={styles["top_footer"]}>
-          <div className={styles["k_footer"]}>
-            <div className={styles["k1_footer"]}>{t("k1_footer")}</div>
-            <div className={styles["m_footer"]}>
-              <div className={styles["m3_footer"]}>{t("m3_footer")}</div>
-              <div className={styles["m4_footer"]}>{t("m4_footer")}</div>
-              <div className={styles["m5_footer"]}>{t("m5_footer")}</div>
+        <div className={styles["footer_mxh"]}>
+          <div className={styles["f1_footer_mxh"]}>
+            {t("f1_footer_mxh")}
+            <div className={styles["f1_mxh_icon"]}>
+              <FacebookShareButton
+                url={shareUrl}
+                quote={title}
+                className="Demo__some-network__share-button"
+              >
+                <FacebookIcon size={45} round />
+              </FacebookShareButton>
+
+              <EmailShareButton
+                url={shareUrl}
+                quote={title}
+                className="Demo__some-network__share-button"
+              >
+                <EmailIcon size={45} round />
+              </EmailShareButton>
+
+              <TwitterShareButton
+                url={shareUrl}
+                quote={title}
+                className="Demo__some-network__share-button"
+              >
+                <TwitterIcon size={45} round />
+              </TwitterShareButton>
+
+              <TelegramShareButton
+                url={shareUrl}
+                quote={title}
+                className="Demo__some-network__share-button"
+              >
+                <TelegramIcon size={45} round />
+              </TelegramShareButton>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles["footer_pt"]}>
+          <div className={styles["top_footer"]}>
+            <div className={styles["k_footer"]}>
+              <div className={styles["k1_footer"]}>{t("k1_footer")}</div>
+              <div className={styles["m_footer"]}>
+                {listPage.map((item) => (
+                  <div className={styles["f3_footer"]}>{item.name}</div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
-      
-    </div>
       <div className={styles["footer_end"]}>
         <div className={styles["f1_end"]}>{t("f1_end")}</div>
         <div className={styles["f2_end"]}>{t("f2_end")}</div>
       </div>
     </div>
-    
-
-  
   );
 };
 
