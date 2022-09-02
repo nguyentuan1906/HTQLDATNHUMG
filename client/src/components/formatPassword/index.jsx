4ff6d8e432fe7bb38e3097b21f22275f95725d5e
thuong8222 <88708605+thuong8222@users.noreply.github.com>
2022-09-02 21:33:09 +0700

    2_9

--- client/src/components/formatPassword/index.jsx
+++ client/src/components/formatPassword/index.jsx
@@ -1,18 +1,20 @@
 import { t } from 'i18next'
 import React from 'react'
 import styles from "./formatPassword.module.scss"
+import { useTranslation } from 'react-i18next';
 const FormatPassword = () => {
+    const { t } = useTranslation();
     return (
         <div className={styles['formatP_content']} >
             <div className={styles['left']}>
-                <h1>Please enter your email to reset your password</h1>
+                <h1>{t('title_fogetPassWord')}</h1>
                 <form className={styles['input']} action="">
-                    <input className={styles['input_yourMail']} type="text" placeholder='Please enter your email' />
-                    <button className={styles['send']}>SEND</button>
+                    <input className={styles['input_yourMail']} type="text" placeholder={t('inputEmail')} />
+                    <button className={styles['send']}>{t('botton_send')}</button>
                 </form>
             </div>
             <div className={styles['right']}>
-                <img src="./images_about/tes2_listen 5. png" alt="images" />
+                <img className={styles['right_images']} src="./images_about/test2_listen 5.png" alt="images" />
 
             </div>
         </div>
