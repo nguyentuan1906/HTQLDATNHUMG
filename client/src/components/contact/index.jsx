dc4f4195cd750430bec6aa2c0866d88273d14a23
duongKDR <106894490+duongKDR@users.noreply.github.com>
2022-09-03 01:29:50 +0700

    fix formartPass

--- client/src/components/contact/index.jsx
+++ client/src/components/contact/index.jsx
@@ -2,50 +2,58 @@ import React from "react";
 import styles from "./contact.module.scss";
 import { useTranslation } from "react-i18next";
 
+const tb =() => {alert(" cám on đã liên hệ với chung tôi")};
 const Contact = () => {
   const { t } = useTranslation();
   return (
-    <div className={styles["contact"]}>
-      <div className={styles["contact__left"]}>
-        {" "}
-        <h1 className={styles["font2"]}>{t("contact")}</h1>
-        <div className={styles["font1"]}>Bạn đang gặp khó khăn cần được hỗ trợ hay cần đóng góp ý kiến cho bộ phận chăm sóc khách hàng? Hãy liên hệ qua bộ phận chăm sóc khách hàng. Chúng tôi sẽ giải quyết vấn đề của bạn nhanh nhất có thể! </div>
-<button className="button-28" role="button">Button 28</button>
-        
-      
-      
-      </div>
+    <div>
+      <h1 className={styles["font2"]}>{t("contact")}</h1>
+      <div className={styles["contact"]}>
+        <div className={styles["contact__left"]}>
+          {" "}
+          <div className={styles["font3_2"]}> Liên hệ trực tiếp</div>
+          <div className={styles["font1"]}>
+            Bạn đang gặp khó khăn cần được hỗ trợ hay cần đóng góp ý kiến cho bộ
+            phận chăm sóc khách hàng? Hãy liên hệ qua bộ phận chăm sóc khách
+            hàng. Chúng tôi sẽ giải quyết vấn đề của bạn nhanh nhất có thể!{" "}
+          </div>
+          <div className={styles["font1_2"]}> số điện thoại:0244554</div>
+          <div className={styles["font1_2"]}> Địa Chỉ: Truong dại học</div>
+        </div>
 
-      <div className={styles["contact__right"]}>
-        <tr>
-          <td>
-          {t("name")}</td>
-          <input
-            type="text"
-            className={styles["Name"]}
-            placeholder={"Họ tên"}
-          />
-        </tr>
-        <tr> <td>
-          {t("phone")}</td>
-          <button
-            className={styles["Phone"]}>
-            
-          </button>
-        </tr>
-        <tr><td>
-          {t("email")}</td>
-          <input
-            type="text"
-            className={styles["Email"]}
-            placeholder={"Email"}
-          />
-        </tr>
-        <tr>
-          {t("Nd")}
-          <textarea type="string" className={styles["Nd"]} rows="7" col="50" />
-        </tr>
-        <button> lien he </button>
+        <div className={styles["contact__right"]}>
+          <div className={styles["font3"]}> Liên hệ với chúng tôi</div>
+          <form className={styles["box"]}>
+            <input
+              className={styles["Name"]}
+              type="text"
+              placeholder={t("name")}
+            />
+            <input
+              className={styles["Phone"]}
+              type="password"
+              placeholder={t("phone")}
+            />
+            <input
+              className={styles["Email"]}
+              type="password"
+              placeholder={t("email")}
+            />
+            <input
+              className={styles["Address"]}
+              type="password"
+              placeholder={t("address")}
+            />
+            <textarea
+              type="string"
+              className={styles[" "]}
+              placeholder={t("import content")}
+              rows="10"
+              col="40"
+            />
+            <button className={styles["button2"]} onClick={"tb"}>{t("give")}</button>
+          </form>
+        </div>
       </div>
     </div>
   );
