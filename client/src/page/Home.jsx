c0dfcd1d3f9210b5be2d69fe1b868639c37de1ba
phuonghole <phuonghole121201@gmail.com>
2022-09-03 19:56:49 +0700

    fix commit trang

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,22 +1,14 @@
 import React from "react";
-<<<<<<< HEAD
+
 import Category from '../components/category/index'
+
 
 const HomePage = () => {
   return (
     <>
       <Category />
-=======
-import News from '../components/news/index'
-import Notice from '../components/notice/index'
-const HomePage = () => {
-  return (
-      <>
-        <News />
-        <Notice />
->>>>>>> 385c445caa30098fc6ce1fdb0dad8ab503798e2e
-      </>
-      );
+    </>
+  );
 };
 
-      export default HomePage;
+export default HomePage;
